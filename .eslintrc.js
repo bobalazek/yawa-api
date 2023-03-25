566d94d696ebb5dd8cf76426bd4cfd1be525cf8b
Borut Balazek <bobalazek124@gmail.com>
2023-03-25 13:55:16 +0100

    Typo

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  root: true,
   env: {
     node: true,
     jest: true,
